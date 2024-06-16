08ec4290f70eab8fd48176d37a47cf77fec61c2a
MysticShadow99 <fixity.autos09@icloud.com>
2024-06-16 20:14:09 +0200

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,7 +1,11 @@
 # counter.py
 
+def increment(counter):
+    return counter + 1
+
 def main():
     counter = 0
+    counter = increment(counter)
     print(f"Counter: {counter}")
 
 if __name__ == "__main__":
