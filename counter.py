b3490a008a5a6d8e7aaef19742988317b83012a6
MysticShadow99 <fixity.autos09@icloud.com>
2024-06-19 20:52:51 +0200

    Update counter.py

--- counter.py
+++ counter.py
@@ -3,9 +3,13 @@
 def increment(counter):
     return counter + 1
 
+def decrement(counter):
+    return counter - 1
+
 def main():
     counter = 0
     counter = increment(counter)
+    counter = decrement(counter)
     print(f"Counter: {counter}")
 
 if __name__ == "__main__":
