18deb7234bb4f2918e9c815bcdb14d92213e1846
MysticShadow99 <fixity.autos09@icloud.com>
2024-09-17 19:36:17 +0200

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,12 +1,12 @@
 # counter.py
 
-def handle_data(action, format, all_counters, history, messages):
-    if action == 'export':
-        export_data(format, all_counters, history)
-        display_message(stdscr, "data_exported", messages)
-    elif action == 'import':
-        import_data(format, all_counters, history)
-        display_message(stdscr, "data_imported", messages)
+def handle_file(action, settings, counter, history, all_counters):
+    if action == 'save':
+        save_data(settings, counter, history, all_counters)
+        display_message(stdscr, "data_saved", messages)
+    elif action == 'load':
+        load_data(settings, counter, history, all_counters)
+        display_message(stdscr, "data_loaded", messages)
     else:
         display_message(stdscr, "invalid_input", messages)
 
@@ -23,7 +23,7 @@ def main(stdscr):
         'e': lambda: handle_data('export', 'csv', all_counters, history, messages),
         'j': lambda: handle_data('export', 'json', all_counters, history, messages),
         'k': lambda: handle_data('import', 'json', all_counters, history, messages),
-        'q': lambda: handle_file_operations("save", settings, counter, history, all_counters)
+        'q': lambda: handle_file('save', settings, counter, history, all_counters)
     }
 
     while True:
