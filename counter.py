61cefa18ddfefd97d10b5e2f1c6ce357927d060a
MysticShadow99 <fixity.autos09@icloud.com>
2024-10-18 18:00:09 +0200

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,8 +1,18 @@
 # counter.py
 
-def display_status(stdscr, program_data):
-    stdscr.addstr(f"Current counter: {program_data[3]['current']}\n")
-    stdscr.addstr(f"Max counter: {program_data[3]['max']}\n")
+def log_action(action, log_file="actions.log"):
+    with open(log_file, 'a') as f:
+        f.write(f"Action executed: {action}\n")
+
+def execute_and_log_action(stdscr, action_key, actions, messages):
+    if action_key in actions:
+        actions[action_key]()
+        display_content(stdscr, [action_key], messages)
+        log_action(action_key)
+
+def process_input_action_and_notifications(stdscr, actions, program_data):
+    action_key = display_and_get_input(stdscr, program_data[1]["menu"])
+    execute_and_log_action(stdscr, action_key, actions, program_data[1])
 
 def main(stdscr):
     program_data, actions = initialize_program_and_actions()
@@ -11,4 +21,3 @@ def main(stdscr):
 
     while True:
         process_input_action_and_notifications(stdscr, actions, program_data)
-        display_status(stdscr, program_data)
