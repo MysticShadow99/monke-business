30ed474ecb7800ccb7a6b38f7c10d5893f23ca74
MysticShadow99 <fixity.autos09@icloud.com>
2024-10-21 21:05:56 +0200

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,13 +1,7 @@
 # counter.py
 
-def save_counter_state(counter, file_name="counter_state.json"):
-    import json
-    with open(file_name, 'w') as f:
-        json.dump(counter, f)
-
-def update_counter_and_save(stdscr, program_data):
+def update_counter(program_data):
     program_data[3]['total'] += 1
-    save_counter_state(program_data[3])
 
 def main(stdscr):
     program_data, actions = initialize_program_and_actions()
@@ -16,4 +10,4 @@ def main(stdscr):
 
     while True:
         process_input_action_and_notifications(stdscr, actions, program_data)
-        update_counter_and_save(stdscr, program_data)
+        update_counter(program_data)
