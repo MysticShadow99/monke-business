aad2fe0ed75436ec72bce2afcb37cb2e54507637
MysticShadow99 <fixity.autos09@icloud.com>
2024-10-25 12:39:36 +0200

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,11 +1,11 @@
 # counter.py
 
-def initialize_program_and_actions():
-    settings = {"notifications": True}
-    messages = {"action_1": "Action 1 executed", "menu": "Select an action:"}
-    counter = {"total": 0, "current": 0, "max": 0}
-    actions = {"action_1": lambda: None}
-    return [settings, messages, counter], actions
+def reset_counter(program_data):
+    program_data[3]['total'] = 0
+
+def display_reset_option(stdscr, program_data):
+    if stdscr.getkey() == 'r':
+        reset_counter(program_data)
 
 def main(stdscr):
     program_data, actions = initialize_program_and_actions()
@@ -14,4 +14,5 @@ def main(stdscr):
 
     while True:
         process_input_action_and_notifications(stdscr, actions, program_data)
+        display_reset_option(stdscr, program_data)
         update_counter(program_data)
