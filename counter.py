ba47b340db6f2c58f08387c0884148d4fd4e9b73
MysticShadow99 <fixity.autos09@icloud.com>
2024-11-11 17:35:07 +0100

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,9 +1,5 @@
 # counter.py
 
-def log_summary(program_data, log_file="summary_log.txt"):
-    with open(log_file, 'a') as f:
-        f.write(f"Total: {program_data[2]['total']}, Max: {program_data[2]['max']}, Notifications: {program_data[0]['notifications']}\n")
-
 def main(stdscr):
     program_data, actions = initialize_program_and_actions()
     if program_data is None:
@@ -13,5 +9,4 @@ def main(stdscr):
         process_input_action_and_notifications(stdscr, actions, program_data)
         display_options(stdscr, program_data)
         update_counter_and_log(program_data)
-        display_summary(program_data)
         log_summary(program_data)
